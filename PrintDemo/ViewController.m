73bef5e7af82ebc41bcf7f84d88487895dcbc44f
tungnd <tung.nd@citigo.net>
2016-10-17 09:35:40 +0700

    Alo

--- PrintDemo/ViewController.m
+++ PrintDemo/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "ESCPOSPrinter.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIPrintInteractionControllerDelegate>
+
+@property (nonatomic, weak) IBOutlet UIImageView *imageView;
+@property (nonatomic, weak) IBOutlet UITextField *textField;
+
+@property (nonatomic, strong) ESCPOSPrinter *escp;
 
 @end
 
@@ -25,5 +31,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)printButtonTapped:(id)sender {
+    NSString * imgfile = [[NSBundle mainBundle] pathForResource:@"sample_6.jpg" ofType:nil];
+    [self.escp printBitmap:imgfile withAlignment:ALIGNMENT_CENTER withSize:BITMAP_NORMAL withBrightness:1];
+    [self.escp cutPaper];
+}
 
+- (IBAction)printText:(id)sender  {
+    [self.escp printString:self.textField.text];
+    [self.escp cutPaper];
+}
+
+- (IBAction)creatConnect:(id)sender {
+    self.escp = [[ESCPOSPrinter alloc] init];
+    NSString *ip = self.textField.text;
+    [self.escp openPort:ip withPortParam:9100];
+}
 @end
---
+++ PrinterLibrary/usr/local/include/POSPrinterFinder.h
@@ -0,0 +1,17 @@
+//
+//  POSPrinterFinder.h
+//  iOS
+//
+//  Created by leesk on 7/9/13.
+//  Copyright (c) 2013. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface POSPrinterFinder : NSObject
+{
+}
+
+- (NSArray *) findWiFiHostList;
+
+@end
